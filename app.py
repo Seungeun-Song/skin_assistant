2a2b6d7b8a08070e88c9804e696a336ae2ef9ed1
Seungeun_Song <lilybad88@gmail.com>
2021-05-27 21:13:26 +0900

    modified app.py

--- app.py
+++ app.py
@@ -1,41 +1,44 @@
-from flask import Flask, redirect, render_template, url_for, request, flash, session
+from flask import Flask, redirect, render_template, url_for, request, flash, session, jsonify
+import requests
+import pymysql
 import sys
 from DB_handler import DBModule
+import os
+
 # import database
 app = Flask(__name__)
-DB = DBModule()
-app.secret_key = "abcdefg"
 
-@app.route("/")
-def login():
-    return render_template("login.html")
 
-@app.route("/login_done", methods=["get"])
-def login_done():
-    uid = request.args.get("id")
-    pwd = request.args.get("pwd")
-    if DB.login(uid, pwd):
-        session["uid"] = uid
-        return redirect(url_for("home"))
-    else:
-        return redirect(url_for("login"))
+# @app.route("/")
+# def login():
+#     return render_template("login.html")
 
-@app.route("/logout")
-def logout():
-    pass
+# # @app.route("/login_done", methods=["get"])
+# # def login_done():
+# #     uid = request.args.get("id")
+# #     pwd = request.args.get("pwd")
+# #     if DB.login(uid, pwd):
+# #         session["uid"] = uid
+# #         return redirect(url_for("home"))
+# #     else:
+# #         return redirect(url_for("login"))
 
-@app.route("/signin")
-def signin():
-    return render_template("signin.html")
+# @app.route("/logout")
+# def logout():
+#     pass
 
-@app.route("/signin_done")
-def signin_done():
-    uid = request.args.get("id")
-    pwd = request.args.get("pwd")
-    if DB.signin(_id_=uid, pwd=pwd):
-        return redirect(url_for("home"))
-    else:
-        return redirect(url_for("signin"))
+# @app.route("/signin")
+# def signin():
+#     return render_template("signin.html")
+
+# @app.route("/signin_done")
+# def signin_done():
+#     uid = request.args.get("id")
+#     pwd = request.args.get("pwd")
+#     if DB.signin(_id_=uid, pwd=pwd):
+#         return redirect(url_for("home"))
+#     else:
+#         return redirect(url_for("signin"))
 
 @app.route("/home")
 def home():
@@ -47,9 +50,9 @@ def home():
 def graph():
     return render_template("Graph.html")
 
-@app.route("/home/photo")
-def photo():
-    return render_template("Photo.html")
+# @app.route("/home/photo")
+# def photo():
+#     return render_template("Photo.html")
     
 @app.route("/home/product")
 def product():
@@ -57,9 +60,39 @@ def product():
 
 
 
+# app.config['IMG_FOLDER'] = os.path.join('static','images')
+@app.route("/home/album")
+def album():
+    test_db = pymysql.connect(user='root', passwd='team09', host='35.180.122.212', port=3306, db='mydb', charset='UTF8')
+    cursor = test_db.cursor(pymysql.cursors.DictCursor)
+    cursor.execute("set names utf8")
+    cursor.execute("SELECT date, img_url1 FROM user_face")
+    rows = cursor.fetchall()
+    test_db.close()
+    
 
+    print(rows)
+    return render_template("album.html", rows=rows)
 
+app.config['IMG_FOLDER'] = os.path.join('static','images')
+@app.route("/home/photo")
+def photo():
+    # import os
+    # file_path = os.path.join('static','images')
+    im = "im"
+    
+    files = os.listdir(os.path.join(app.config['IMG_FOLDER'], im))
+    #files = os.path.join(file_path, files)
+    #files.sort()
+    # file_list = []
+    # for file in files:
+    #     file = os.path.join(file_path, file)
+    #     file_list.append(file)
 
+    return render_template("Photo.html", pics=files, im=im)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
+
+    
+    
